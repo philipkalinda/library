a292bed7252a2c7c68f171a8167290d18bee8346
Philip Kalinda <philipkalinda@gmail.com>
2017-04-16 17:36:47 +0100

    stats module update

--- statistics/stats.py
+++ statistics/stats.py
@@ -7,9 +7,9 @@ class Stats():
 
 	def median(x):
 		if len(x) % 2 != 0:
-			return sorted(list(x))[(len(x)+1)/2]
+			return sorted(list(x))[int((len(x)+1)/2)]
 		else:
-			return sum(sorted(list(x))[(len(x)/2):(len(x)/2)+2])/2
+			return sum(sorted(list(x))[int((len(x)/2)):int((len(x)/2)+2)])/2
 
 	def mode(x):
 		mode_val = 0; mode_count = 0;
