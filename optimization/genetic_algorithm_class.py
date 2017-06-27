9042253c3b0dd92af5026b99180406550e3062c8
Philip Kalinda <philipkalinda@gmail.com>
2017-06-27 16:59:53 +0100

    genetic algorithm optimisation class

--- optimization/genetic_algorithm_class.py
+++ optimization/genetic_algorithm_class.py
@@ -4,21 +4,18 @@
 
 #Required Imports
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+import time
 from sklearn.model_selection import cross_val_score, KFold
 
 
-class FeatureSelectionGeneticAlgorithm:
+class FeatureSelectionGeneticAlgorithm():
     """Built to be compatible with sci-kit learn library for both regression and classification models
     This is designed to help with feature selection in highly dimensional datasets
     """
-    def __init__(self,mutation_rate = 0.001, iterations = 250, pool_size = 50):
+    def __init__(self, mutation_rate = 0.001, iterations = 100, pool_size = 50):
         # imports
-        import numpy as np
-        import pandas as pd
-        import matplotlib.pyplot as plt
-        from sklearn.model_selection import cross_val_score, KFold
+        # import numpy as np
+        # from sklearn.model_selection import cross_val_score, KFold
         # self initiations
         self.mutation_rate = mutation_rate
         self.iterations = iterations
@@ -28,6 +25,8 @@ class FeatureSelectionGeneticAlgorithm:
         self.kf = KFold(n_splits=5)
 
     def results(self):
+        """Print best results from the fit
+        """
         return self.pool[0]
         pass
 
@@ -37,27 +36,29 @@ class FeatureSelectionGeneticAlgorithm:
         y = Y output data
         """
         # reset data in case run before (iterations_results hoted as a class variable and must be reset when refitting the Algorithm)
-        __init__(self.mutation_rate, self.iterations, self.pool_size)
+
+        # __init__(self.mutation_rate, self.iterations, self.pool_size)
+        
         X = np.array(X)
-        # - legacy - # self.pool = [[np.random.randint(0,2) for genes in range(X.shape[1])] for dna in range(self.pool_size)]
         self.pool = np.random.randint(0,2,(self.pool_size, X.shape[1]))
 
         for iteration in range(1,self.iterations+1):
+            s_t = time.time()
             scores = list(); fitness = list(); 
             for dna in self.pool:
                 chosen_idx = [idx for bit, idx in zip(dna,range(X.shape[1])) if bit==1]
                 adj_X = X[:,chosen_idx]
 
-                if _type = 'regression':
-                    score = np.mean(cross_val_score(model, X, y, scoring='r2', cv=self.kf))
+                if _type == 'regression':
+                    score = np.mean(cross_val_score(model, adj_X, y, scoring='r2', cv=self.kf))
 
-                elif _type = 'classification':
-                    score = np.mean(cross_val_score(model, X, y, scoring='accuracy', cv=self.kf))
+                elif _type == 'classification':
+                    score = np.mean(cross_val_score(model, adj_X, y, scoring='accuracy', cv=self.kf))
 
                 scores.append(score)
             fitness = [x/sum(scores) for x in scores]
 
-            fitness, self.pool, scores = (list(t) for t in zip(*sorted(zip(fitness, self.pool, scores),reverse=True)))
+            fitness, self.pool, scores = (list(t) for t in zip(*sorted(zip(fitness, [list(l) for l in list(self.pool)], scores),reverse=True)))
 
             # storage of iteration results
             self.iterations_results['{}'.format(iteration)] = dict()
@@ -65,10 +66,11 @@ class FeatureSelectionGeneticAlgorithm:
             self.iterations_results['{}'.format(iteration)]['pool'] = self.pool
             self.iterations_results['{}'.format(iteration)]['scores'] = scores
 
+            self.pool = np.array(self.pool)
 
             if iteration != self.iterations+1:
                 new_pool = []
-                for dna in self.pool[1:,int((len(self.pool)/2)+1)]:
+                for dna in self.pool[1:int((len(self.pool)/2)+1)]:
                     random_split_point = np.random.randint(1,len(dna))
                     next_gen1 = np.concatenate((self.pool[0][:random_split_point], dna[random_split_point:]), axis = 0)
                     next_gen2 = np.concatenate((dna[:random_split_point], self.pool[0][random_split_point:]), axis = 0)
@@ -83,6 +85,8 @@ class FeatureSelectionGeneticAlgorithm:
                 self.pool = new_pool
             else:
                 continue
+            e_t = time.time()
+            print('Iteration {} Complete [Time Taken: {} Seconds]'.format(iteration,round(e_t-s_t,2) ))
         
     
 
